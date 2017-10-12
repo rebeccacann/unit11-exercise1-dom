d621c5121750b6a6a21b95613a5ccde824f356a0
rebulba <30329874+rebulba@users.noreply.github.com>
2017-10-12 12:55:01 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.querySelectorAll('.current')[1].textContent = "The Violent Bear It Away"; 
+document.getElementsById('next').textContent = "Me Talk Pretty One Day";  
+document.querySelector('li').textContent = "Brothers Karamazov";
+document.getElementsByTagName('li')[3].textContent = "JavaScript is Fun!";
